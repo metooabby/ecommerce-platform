651aaa9eb92f91fbb72e84e7845ed14bf75f8e7d
Anurag Bhelsewale <anuragbhelsewale21@gmail.com>
2026-01-08 13:41:11 +0530

    chore: add eslint and prettier configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "no-console": "off"
+  }
+};
